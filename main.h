6cfddd8025b007312e71e60cc4529fd095c7a32e
Alula65 <alulasema@gmail.com>
2023-04-27 04:37:01 -0700

    printf

--- main.h
+++ main.h
@@ -104,6 +104,8 @@ int write_num(int ind, char bff[], int flags, int width, int precision,
 		int length, char padd, char extra_c);
 int write_pointer(char buffer[], int ind, int length,
 		int width, int flags, char padd, char extra_c, int padd_start);
+int write_unsgnd(int is_negative, int ind, char buffer[],
+		int flags, int width, int precision, int size);
 
 /****************** UTILS ******************/
 int is_printable(char);
@@ -111,7 +113,6 @@ int append_hexa_code(char, char[], int);
 int is_digit(char);
 
 long int convert_size_number(long int num, int size);
-long int convert_size_unsgnd(unsigned long int num,
-		int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
